dd271c2fb87402bbf96bd46138c60f0c262430c5
Lucas Seixas <lucasseixaslima2@gmail.com>
2023-05-29 00:42:21 -0300

    exercicios unidade 3

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/');
+var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
+  .then(resultado => resultado.json())
+  .then(e => {
+    if (e.erro) {
+      throw 'cep não existe';
+    } else console.log(e);
+  })
+  .catch(error => console.log(error))
+  .finally(() => console.log('Processamento concluido'));
 
 console.log(consultaCep);
