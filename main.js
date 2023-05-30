699ac99437eb4b75bcffec5364f942e3e0e105c1
Lucas Seixas <lucasseixaslima2@gmail.com>
2023-05-30 11:03:19 -0300

    exercicios unidade 5

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+var paragrafoDeErro = document.createElement('p');
+
 async function buscaEndereco(cep) {
+  var mensagemErro = document.getElementById('erro');
+  mensagemErro.innerHTML = '';
   try {
     var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
     var consultaCepConvertido = await consultaCep.json();
@@ -6,12 +10,23 @@ async function buscaEndereco(cep) {
       throw 'CEP não existe!!';
     }
     console.log(consultaCepConvertido);
+
+    var logradouro = document.getElementById('endereco');
+    var bairro = document.getElementById('bairro');
+    var cidade = document.getElementById('cidade');
+    var estado = document.getElementById('estado');
+
+    logradouro.value = consultaCepConvertido.logradouro;
+    bairro.value = consultaCepConvertido.bairro;
+    cidade.value = consultaCepConvertido.localidade;
+    estado.value = consultaCepConvertido.uf;
   } catch (error) {
+    paragrafoDeErro.innerHTML = 'CEP invalido, favor digitar o CEP correto!';
+    mensagemErro.appendChild(paragrafoDeErro);
     console.log(error);
   }
 }
 
-let ceps = ['01001000', '01001001'];
-let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
-console.log(conjuntoCeps);
-Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
+document
+  .getElementById('cep')
+  .addEventListener('focusout', e => buscaEndereco(e.target.value));
