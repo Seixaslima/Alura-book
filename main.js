428d7880dc87a49c724a09fcdc5a8542dbd78701
Lucas Seixas <lucasseixaslima2@gmail.com>
2023-05-30 12:58:14 -0300

    adicionado funcionalidade de auto completar

--- main.js
+++ main.js
@@ -1,32 +1,86 @@
-var paragrafoDeErro = document.createElement('p');
+const idsEnderecoFormulario = [
+  'cep',
+  'endereco',
+  'numero',
+  'complemento',
+  'bairro',
+  'cidade',
+  'estado'
+];
+
+var camposEnderecoFormulario = {};
+
+idsEnderecoFormulario.forEach(e => {
+  camposEnderecoFormulario[e] = document.getElementById(e);
+});
+
+const paragrafoDeErro = document.createElement('p');
+paragrafoDeErro.classList.add('erro__texto');
+paragrafoDeErro.innerHTML += 'CEP invalido, favor digitar o CEP correto!';
+const mensagemErro = document.getElementById('erro');
+
+camposEnderecoFormulario['cep'].addEventListener('focusout', e => {
+  const cep = e.target.value;
+  resetaEndereco();
+  esperaPorCep();
+  if (cep != '') {
+    if (cep.length == 8) {
+      buscaEndereco(e.target.value);
+    } else {
+      mensagemErro.appendChild(paragrafoDeErro);
+    }
+  }
+});
+
+esperaPorCep();
 
 async function buscaEndereco(cep) {
-  var mensagemErro = document.getElementById('erro');
-  mensagemErro.innerHTML = '';
+  esperaPorCep();
   try {
     var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
     var consultaCepConvertido = await consultaCep.json();
     if (consultaCepConvertido.erro) {
       throw 'CEP não existe!!';
     }
-    console.log(consultaCepConvertido);
 
-    var logradouro = document.getElementById('endereco');
-    var bairro = document.getElementById('bairro');
-    var cidade = document.getElementById('cidade');
-    var estado = document.getElementById('estado');
-
-    logradouro.value = consultaCepConvertido.logradouro;
-    bairro.value = consultaCepConvertido.bairro;
-    cidade.value = consultaCepConvertido.localidade;
-    estado.value = consultaCepConvertido.uf;
+    completaEndereco(consultaCepConvertido);
   } catch (error) {
-    paragrafoDeErro.innerHTML = 'CEP invalido, favor digitar o CEP correto!';
     mensagemErro.appendChild(paragrafoDeErro);
-    console.log(error);
   }
 }
 
-document
-  .getElementById('cep')
-  .addEventListener('focusout', e => buscaEndereco(e.target.value));
+function esperaPorCep() {
+  mensagemErro.innerHTML = '';
+  resetaEndereco();
+  idsEnderecoFormulario.forEach(e => {
+    if (e === 'estado') {
+      camposEnderecoFormulario[e].setAttribute('disabled', '');
+    } else {
+      camposEnderecoFormulario[e].setAttribute('readonly', '');
+    }
+  });
+  camposEnderecoFormulario['cep'].removeAttribute('readonly');
+}
+
+function resetaEndereco() {
+  idsEnderecoFormulario.forEach(e => {
+    if (e != 'cep') {
+      camposEnderecoFormulario[e].value = '';
+    }
+  });
+}
+
+function completaEndereco(endereco) {
+  camposEnderecoFormulario['endereco'].value = endereco.logradouro;
+  if (endereco.logradouro === '') {
+    camposEnderecoFormulario['endereco'].removeAttribute('readonly');
+  }
+  camposEnderecoFormulario['bairro'].value = endereco.bairro;
+  if (endereco.bairro === '') {
+    camposEnderecoFormulario['bairro'].removeAttribute('readonly');
+  }
+  camposEnderecoFormulario['cidade'].value = endereco.localidade;
+  camposEnderecoFormulario['estado'].value = endereco.uf;
+  camposEnderecoFormulario['numero'].removeAttribute('readonly');
+  camposEnderecoFormulario['complemento'].removeAttribute('readonly');
+}
